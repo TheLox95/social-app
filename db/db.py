9bb012278db057a452272605a9eb265bb44879d9
leo_9_t <leo_9_t@hotmail.com>
2026-07-08 18:10:38 -0500

    CLEAN up on db file

--- db/db.py
+++ db/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Session
 
-engine = create_engine("sqlite:///example.db")
+engine = create_engine("sqlite:///example.db",
+                       pool_size=5,max_overflow=10,pool_timeout=30,pool_recycle=3600,pool_pre_ping=True)
 
 class DBModel(DeclarativeBase):
     pass
 
-
+def get_db():
+    with Session(engine) as session:
+        yield session
--- routes/users.py
+++ routes/users.py
@@ -1,9 +1,5 @@
-
-import os
-
 from fastapi import Depends
 from fastapi.routing import APIRouter
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from db import db
@@ -13,10 +9,7 @@ from services.auth import get_token_data
 router = APIRouter(tags=["auth"])
 
 @router.get("/profile")
-def login(usr: dict[str, int] = Depends(get_token_data)):
-    with Session(db.engine) as session:
-        query = select(User).where(User.id == usr.get("id"))
+def login(usr: dict[str, int] = Depends(get_token_data), driver: Session = Depends(db.get_db)):
+    user = driver.query(User).where(User.id == usr.get("id")).first()
 
-        user = session.scalars(query).first()
-
-        return user
+    return user
