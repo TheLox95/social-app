7e5501e0bf12d45aabd80c10c914f4a82066aa68
leo_9_t <leo_9_t@hotmail.com>
2026-07-08 20:28:24 -0500

    ADDED migrations

--- models/User.py
+++ models/User.py
@@ -1,15 +1,19 @@
-from sqlalchemy import String
+from datetime import datetime
+from sqlalchemy import String, func
 from sqlalchemy.orm import mapped_column, Mapped
 from db import db
 
 class User(db.DBModel):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement="auto")
     fullname: Mapped[str] = mapped_column(String(35))
     password: Mapped[str] = mapped_column(String(256))
     email: Mapped[str] = mapped_column(String())
-    username: Mapped[str] = mapped_column(String(), unique=True)
+    username: Mapped[str] = mapped_column(String(18), unique=True)
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
 
 
 db.DBModel.metadata.create_all(db.engine)
